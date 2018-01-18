1f96fdfba0e124e75d412d17f730cc45d288b3dd
anuu11 <35534904+anuu11@users.noreply.github.com>
2018-01-18 19:35:52 +0530

    Add files via upload

---
+++ anu15.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main() {
+int num,n,m;
+printf("enter numbers");
+scanf("%d%d",&m,&n);
+
+  for (num = m+1; num <n; num++)
+
+        {
+
+             if (num % 2 == 0)
+
+                  printf ("%d ", num);
+
+         }
+
+             }
